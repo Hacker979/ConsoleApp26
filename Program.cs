7dc406b98f87d780a5b3870cf30f56ebac363cb8
Kab-35-16 <Kab-35-16@KAB-31-11->
2025-02-19 09:43:06 +0200

    bg

--- Program.cs
+++ Program.cs
@@ -8,31 +8,31 @@ namespace ConsoleApp26
 {
     class Program
     {
-        static void Print(int[] mas, int min, ref int b)
+        static void Print(int[] mas,ref int b)
         {
             foreach (int i in mas)
             {
                 Console.Write(i + " ");
             }
-            Console.WriteLine($"\nчисел больше {min} = {b}");
+            Console.WriteLine($"\nминимальное = {b}");
         }
-        static void Write(int[] mas, int min, ref int b)
+        static void Write(int[] mas, ref int b)
         {
+            
             for (int i = 0; i < mas.Length; i++)
             {
-                if (mas[i] >= min)
+                if (mas[i] < b)
                 {
-                    b++;
+                    b = mas[i];
                 }
             }
         }
         static void Mas()
         {
-            int min = int.Parse(Console.ReadLine());
-            int b = 0;
             int[] mas = new int[5] {4, 5, 6, 3, 7 } ;
-            Write( mas, min, ref b);
-            Print(mas, min, ref b);
+            int b = mas[0];
+            Write( mas, ref b);
+            Print( mas, ref b);
 
         }
         static void Main(string[] args)
