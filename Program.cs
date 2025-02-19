55e5242d1a6d2a7a1715509287bad7bb15c5cbe8
Kab-35-16 <Kab-35-16@KAB-31-11->
2025-02-19 09:22:51 +0200

    asf

--- Program.cs
+++ Program.cs
@@ -8,29 +8,36 @@ namespace ConsoleApp26
 {
     class Program
     {
-        static void Print(int[] mas)
+        static void Print(int[] mas, ref int b)
         {
             foreach (int i in mas)
             {
                 Console.Write(i + " ");
             }
+            Console.WriteLine($"Четных = {b}");
         }
-        static void Write(int a, int[] mas)
+        static void Write(int a, int[] mas, ref int b)
         {
             Random rand = new Random();
+            
             for (int i = 0; i < a; i++)
             {
                 mas[i] = rand.Next(-10, 10);
                 Console.WriteLine($"mas[{i}] = {mas[i]}");
-                
+
+                if (mas[i] % 2 == 0)
+                {
+                    b++;
+                }
             }
         }
         static void Mas()
         {
             int a = int.Parse(Console.ReadLine());
+            int b = 0;
             int[] mas = new int[a];
-            Write(a, mas);
-            Print(mas);
+            Write(a, mas, ref b);
+            Print(mas, ref b);
 
         }
         static void Main(string[] args)
