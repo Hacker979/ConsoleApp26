70f8be39d009f4dec4d3ff78daa93fd9ae824ff1
Kab-35-16 <Kab-35-16@KAB-31-11->
2025-02-19 08:49:44 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void mas(int a)
+        {
+            int[] mas = new int[a];
+            for (int i = 0; i<a; i++)
+            {
+                Console.Write($"mas[{i}] = ");
+                mas[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int i in mas)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            mas(int.Parse(Console.ReadLine()));
+
+            Console.ReadLine();
+        }
+    }
+}
