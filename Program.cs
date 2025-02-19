cdbfb9fed08be6ca976d6e84df38838a12ae3033
Kab-35-16 <Kab-35-16@KAB-31-11->
2025-02-19 09:13:44 +0200

    fDSAE

--- Program.cs
+++ Program.cs
@@ -8,22 +8,34 @@ namespace ConsoleApp26
 {
     class Program
     {
-        static void mas(int a)
+        static void Print(int[] mas)
         {
-            Random rand = new Random();
-            int[] mas = new int[a];
-            for (int i = 0; i<a; i++)
-            {
-                mas[i] = rand.Next(-10,10);
-            }
             foreach (int i in mas)
             {
                 Console.Write(i + " ");
             }
         }
+        static void Write(int a, int[] mas)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < a; i++)
+            {
+                mas[i] = rand.Next(-10, 10);
+                Console.WriteLine($"mas[{i}] = {mas[i]}");
+                
+            }
+        }
+        static void Mas()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int[] mas = new int[a];
+            Write(a, mas);
+            Print(mas);
+
+        }
         static void Main(string[] args)
         {
-            mas(int.Parse(Console.ReadLine()));
+            Mas();
 
             Console.ReadLine();
         }
